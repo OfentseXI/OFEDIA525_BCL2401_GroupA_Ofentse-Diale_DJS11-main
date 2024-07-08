31dc4016c627d52333636d175a5e2c3761c8bb91
OfentseXI <dofentse69@gmail.com>
2024-07-08 15:23:29 +0200

    Removed onClick handler from search icon and added onClick handlers to Favorites icon and arrow icon

--- MyCast/src/components/Sidebar.jsx
+++ MyCast/src/components/Sidebar.jsx
@@ -14,18 +14,18 @@ const Sidebar = () => {
                 <p className='font-bold'>Home</p>
             </div>
             <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-                <img onClick={()=>navigate('/')}className='w-6' src={assets.search_icon} alt="" />
+                <img className='w-6' src={assets.search_icon} alt="" />
                 <p className='font-bold'>Search</p>
             </div>
         </div>
         <div className='bg-[#121212] h-[85%] rounded'>
             <div className='p-4 flex items-center justify-between'>
                 <div className='flex items-center gap-3'>
-                    <img className='w-8'src={assets.stack_icon} alt="" />
-                    <p className='font-semibold'>Favorites</p>
+                    <img onClick={() => navigate ('/favorites')} className='w-8'src={assets.stack_icon} alt="" />
+                    <p className='font-semibold'>Favorites</p> 
                 </div>
                 <div className='flex items-center gap-3'>
-                    <img className= 'w-5' src={assets.arrow_icon} alt="" />
+                    <img onClick={() => navigate ('/favorites')} className= 'w-5' src={assets.arrow_icon} alt="" />
                     <img className= 'w-5' src={assets.plus_icon} alt="" />
                 </div>
             </div>
