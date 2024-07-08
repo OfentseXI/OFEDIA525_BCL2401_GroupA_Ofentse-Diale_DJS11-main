d9b85d0a4c3889f91cc32e0f9c5a8ad75666d9a7
OfentseXI <dofentse69@gmail.com>
2024-07-08 14:02:00 +0200

    first commit

---
+++ MyCast/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import Sidebar from './components/Sidebar'
+import AudioPlayer from './components/AudioPlayer'
+import Display from './components/Display'
+import { AudioPlayerContext } from './context/AudioPlayerContext'
+
+const App = () => {
+
+  const {audioRef,track} = useContext(AudioPlayerContext)
+
+  return (
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+        <Display />
+      </div>
+      <AudioPlayer />
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
+    </div>
+  )
+}
+
+export default App
