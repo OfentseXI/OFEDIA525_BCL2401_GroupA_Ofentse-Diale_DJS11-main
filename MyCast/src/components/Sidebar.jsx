3353e5de39633a9f104faca5e254ccfbb1989555
OfentseXI <dofentse69@gmail.com>
2024-07-08 15:29:42 +0200

    Added onClick event to "Favorites" paragraph in Sidebar component.

--- MyCast/src/components/Sidebar.jsx
+++ MyCast/src/components/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
             <div className='p-4 flex items-center justify-between'>
                 <div className='flex items-center gap-3'>
                     <img onClick={() => navigate ('/favorites')} className='w-8 cursor-pointer'src={assets.stack_icon} alt="" />
-                    <p className='font-semibold cursor-pointer'>Favorites</p> 
+                    <p onClick={() => navigate ('/favorites')} className='font-semibold cursor-pointer'>Favorites</p> 
                 </div>
                 <div className='flex items-center gap-3'>
                     <img onClick={() => navigate ('/favorites')} className= 'w-5 cursor-pointer' src={assets.arrow_icon} alt="" />
