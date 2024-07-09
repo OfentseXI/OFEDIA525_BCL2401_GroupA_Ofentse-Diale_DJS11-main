d4db723cfe3b75c8c56443e27e304810c18ea68c
OfentseXI <dofentse69@gmail.com>
2024-07-09 15:45:52 +0200

    Added a new home icon to the Navbar component that navigates to the root route when clicked.

--- MyCast/src/components/Navbar.jsx
+++ MyCast/src/components/Navbar.jsx
@@ -5,11 +5,13 @@ import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
 
   const navigate = useNavigate()
+  
 
   return (
     <>
       <div className='w-full flex justify-between items-center font-semibold'>
         <div className='flex items-center gap-2'>
+            <img onClick={() => navigate('/')} className='w-8 bg-black p-2 rounded-2xl lg:hidden cursor-pointer hover:bg-purple-500'src={assets.home_icon} alt="" />
             <img onClick={() => navigate(-1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer hover:bg-purple-500' src={assets.arrow_left} alt="" />
             <img onClick={() => navigate(+1)} className='w-8 bg-black p-2 rounded-2xl cursor-pointer hover:bg-purple-500' src={assets.arrow_right} alt="" />
         </div>
