29131b2f02f1f5180928545f6d1e02413750d09d
OfentseXI <dofentse69@gmail.com>
2024-08-23 13:01:48 +0200

    Added new genres.jsx file and loaded it with the genres array

---
+++ MyCast/src/components/Genres.jsx
@@ -0,0 +1,12 @@
+const genres = [
+    { id: 0, name: 'All' },
+    { id: 1, name: 'Personal Growth' },
+    { id: 2, name: 'Investigative Journalism' },
+    { id: 3, name: 'History' },
+    { id: 4, name: 'Comedy' },
+    { id: 5, name: 'Entertainment' },
+    { id: 6, name: 'Business' },
+    { id: 7, name: 'Fiction' },
+    { id: 8, name: 'News' },
+    { id: 9, name: 'Kids and Family' },
+  ];
