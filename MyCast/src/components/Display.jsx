90429a7a154b875718c5dd0b174f8602a3115ed6
OfentseXI <dofentse69@gmail.com>
2024-08-23 16:17:51 +0200

    Added Home component to Routes in Display.jsx

--- MyCast/src/components/Display.jsx
+++ MyCast/src/components/Display.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import PodcastList from './PodcastList'; 
 import Favorites from './Favorites';
 import SeriesDetail from './SeriesDetail'; 
+import Home from './Home';
 
 
 const Display = () => {
     return (
         <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
             <Routes>
+                <Route path="/" element={<Home />} />
                 <Route path="/podcasts" element={<PodcastList />} />
                 <Route path="/favorites" element={<Favorites />} /> 
                 <Route path="/series/:id" element={<SeriesDetail />} />
